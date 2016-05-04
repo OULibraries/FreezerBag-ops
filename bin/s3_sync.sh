f6af27f11804cdd903474754e41696f560617b81
Jason Sherman <jsn.sherman@gmail.com>
2016-05-04 15:24:52 -0500

    First swipe at s3 sync.

---
+++ bin/s3_sync.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+# Require arguments
+if [ ! -z "$1" ]
+then
+  SOURCE=$1
+else
+  echo "Requires LocalPath as source."
+  exit 1;
+fi
+
+if [ ! -z "$2" ]
+then
+  DEST=$2
+else
+  echo "Requires LocalPath OR S3Uri as destination."
+  exit 1;
+fi
+
+if [[ ! -z "$3" ]]; then
+  TYPE=$3
+  if [[ "$TYPE" -ne 'bag' || "$TYPE" -ne 'dir' ]]
+ then
+    echo "Requires type of bag or dir."
+    exit 1;
+  fi
+else
+  echo "Requires type of bag or dir."
+  exit 1;
+fi
+
+if [ ! -z "$4" ]
+then
+  MAILTO=$4
+else
+  echo "Requires email address as argument"
+  exit 1;
+fi
+
+# CC email is optional
+if [ ! -z "$5" ]
+then
+  MAILCC="-c $5"
+else
+  MAILCC=""
+fi
+
+SYNCOPTS="--dryrun"
+
+BASENAME=`basename ${SOURCE}`
+
+# Source exists. This currently only works for LocalPath sources
+if [[ -d ${SOURCE} ]] ; then
+
+# Make a lock file to prevent runs from overlapping
+FLOCK=/var/lock/${BASENAME}.lock
+ 
+# Make a named pipe to hold the email contents.
+# We go to the trouble for cron compatibility
+PIPE=/tmp/${BASENAME}.pipe
+
+(
+  flock -x -w 10 200 || exit 1
+
+  # Holds our email message body
+  BODY=''
+  
+  # Create the pipe on start
+  if [[ ! -p $PIPE ]]; then
+    mkfifo $PIPE
+  fi
+  
+  # Attach a file descriptor.
+  # This causes the shell to buffer the pipe so we avoid worries about blocking
+  exec 3<>$PIPE
+
+  # Get the time
+  NOW=`date`
+  
+  echo $NOW>&3
+
+  # Skip this source if it is invalid
+  # This currently only works for LocalPath sources
+
+  # If you were running this against bags, you might want this.  
+  if [[ "$TYPE" == "bag" ]]; then
+    bagit.py --validate $SOURCE 2>&3
+    SOURCESTATUS="$?"
+  # This is basically a null test, since we already established this is a dir
+  elif [[ "$TYPE" == "dir" ]]; then
+    stat --format=%F:\ %n $SOURCE >&3 2>&3
+    SOURCESTATUS="$?"
+  fi
+
+  if [ "$SOURCESTATUS" -gt 0  ]; then
+    echo 'EOF'>&3
+    DEAD=false
+    until $DEAD; do
+      read -u 3 LINE || DEAD=true
+      if [[ "$LINE" == 'EOF' ]]; then
+        break
+      fi
+      # Append body
+      BODY=${BODY}$'\n'${LINE}
+    done
+
+    # Get the time
+    NOW=`date`
+  
+    echo "$BODY" | mail -s "s3sync source invalid: $BASENAME $NOW" $MAILCC $MAILTO
+  else
+    # If it is a valid bag, sync it.
+    SYNC=`aws s3 sync ${SYNCOPTS} ${SOURCE} ${DEST}`
+    DEAD=false
+
+    # Print the output to our pipe in a subshell
+    printf "${SYNC}\nEOF\n" >&3 2>&3 &
+
+    until $DEAD; do
+      read -u 3 LINE || DEAD=true
+      if [[ $LINE == 'EOF' ]]; then
+        break
+      fi
+      #echo "$LINE"
+      # Append body
+      BODY=${BODY}$'\n'${LINE}
+    done
+
+    # Get the time
+    NOW=`date`
+  
+    echo "$BODY" | mail -s "s3sync: $BASENAME $NOW" $MAILCC $MAILTO
+  fi
+) 200>$FLOCK
+fi
+
+function finish {
+  # Kill the pipe on exit
+  rm $PIPE
+  # Kill the lock on exit
+  rm $FLOCK
+}
+
+trap finish EXIT
